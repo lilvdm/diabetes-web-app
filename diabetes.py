5d4725bbf2128963fa05968ae6b1c8f26460a4ec
lilvdm <lil.vdm97@gmail.com>
2026-03-24 01:30:44 +0900

    Add files via upload

--- diabetes.py
+++ diabetes.py
@@ -13,6 +13,16 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
 bootstrap5 = Bootstrap5(app)
 
+# --- GLOBAL INITIALIZATION (Only happens once) ---
+# 1. Load the model globally
+model = keras.models.load_model('pima_model.keras')
+
+# 2. Setup the scaler globally so we don't read the CSV on every click
+data = pd.read_csv('./diabetes.csv', sep=',')
+X = data.values[:, 0:8]
+scaler = MinMaxScaler()
+scaler.fit(X)
+
 class LabForm(FlaskForm):
     preg = StringField('# Pregnancies', validators=[DataRequired()])
     glucose = StringField('Glucose', validators=[DataRequired()])
@@ -33,26 +43,24 @@ def index():
 def lab():
     form = LabForm()
     if form.validate_on_submit():
+        # Process input
         X_test = np.array([[float(form.preg.data), float(form.glucose.data),
                           float(form.blood.data), float(form.skin.data),
                           float(form.insulin.data), float(form.bmi.data),
                           float(form.dpf.data), float(form.age.data)]])
         
-        data = pd.read_csv('./diabetes.csv', sep=',')
-        X = data.values[:, 0:8]
-        scaler = MinMaxScaler()
-        scaler.fit(X)
-        X_test = scaler.transform(X_test)
+        # Scale data using the global scaler
+        X_test_scaled = scaler.transform(X_test)
         
-        model = keras.models.load_model('pima_model.keras')
-        prediction = model.predict(X_test)
+        # Run prediction using the global model
+        prediction = model.predict(X_test_scaled)
         res = prediction[0][0]
         res = np.round(res * 100, 2)
         
         return render_template('result.html', res=res)
+    
     return render_template('prediction.html', form=form)
 
 if __name__ == '__main__':
-    # Get port from environment variable, default to 5000 for local testing
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
